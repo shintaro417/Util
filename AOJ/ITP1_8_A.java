c37beb56893558658e2b741204f8f4c95a28412a
nuko1129 <shintaro11417@gmail.com>
2021-05-07 15:46:02 +0900

    大文字と小文字を反転させて出力する

---
+++ AOJ/ITP1_8_A.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+
+		str = CaseReverse(str);
+
+		System.out.printf("%s", str);
+		System.out.println();
+
+		sc.close();
+	}
+
+	/**
+	 * @param s
+	 * @return String 大文字、小文字を反転した文字列を返す
+	 */
+	static String CaseReverse(String s) {
+		StringBuilder sBuilder = new StringBuilder();
+		for(int i = 0;i < s.length();i++) {
+			char c = s.charAt(i);
+
+			//文字が大文字か否か
+			if(Character.isUpperCase(c)) {
+				//32を足すと小文字になる
+				c += 32;
+				sBuilder.append(c);
+			}else if(Character.isLowerCase(c)) {
+				c -= 32;
+				sBuilder.append(c);
+			}else {
+				sBuilder.append(c);
+			}
+		}
+
+		return sBuilder.toString();
+	}
+}
