232119ece93f94efb06fd70545450211cf39c30b
nuko1129 <shintaro11417@gmail.com>
2021-05-25 10:30:57 +0900

    ２点間の距離を求めるメソッド(コメント追加)

--- Lib/DistanceBetweenTwoPoints.java
+++ Lib/DistanceBetweenTwoPoints.java
@@ -7,6 +7,14 @@ public class Main {
 		sc.close();
 	}
 
+	/**
+	 * 2点間の距離を求めるメソッド
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @return double
+	 */
 	public static double distance(double x1,double y1,double x2,double y2) {
 		double distance = Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2);
 
