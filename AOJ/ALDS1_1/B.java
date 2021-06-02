196431a77a245b66e7c2bb9196b6ba8a2d7b6e6b
nuko1129 <shintaro11417@gmail.com>
2021-06-02 10:10:28 +0900

    最大公約数を求める

---
+++ AOJ/ALDS1_1/B.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+
+		System.out.println(gcd(x, y));
+
+
+
+		sc.close();
+	}
+
+	public static int gcd(int x,int y) {
+		int gcd = 0;
+		int r = 0;
+		if(x >= y) {
+			while(true) {
+				r = x % y;
+				if(r == 0) {
+					gcd = y;
+					break;
+				}
+				x = y;
+				y = r;
+			}
+		}else {
+			while(true) {
+				r = y % x;
+				if(r == 0) {
+					gcd = x;
+					break;
+				}
+				y = x;
+				x = r;
+			}
+		}
+		return gcd;
+	}
+}
+
